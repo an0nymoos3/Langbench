47392a0e3d03ec5d15fafd96682fc840b39d5729
an0nymoos3 <moosely123@gmail.com>
2023-12-22 23:16:03 +0100

    prettier benchmarking

--- scripts/primefinder_benchmark.sh
+++ scripts/primefinder_benchmark.sh
@@ -7,54 +7,109 @@ read max_num
 clear
 
 # -- Java --
+clear
 echo "Compiling Java..."
+echo "[>                ]"
 cd java/primefinder/src/
-javac Main.java
+javac Main.java 
 
+clear
 echo "Running Java benchmark..."
+echo "[=>               ]"
 java Main $max_num > /dev/null
+
+clear
+echo "Running Java benchmark..."
+echo "[==>              ]"
 java Main $max_num > /dev/null
+
+clear
+echo "Running Java benchmark..."
+echo "[===>             ]"
 \time -o ../../../java.txt -f "%C\nTime: %e\n%P\n" java Main $max_num
 
 cd ../../../
 
 # - Rust --
+clear
 echo "Compiling Rust benchmark..."
-sleep 1
-
+echo "[====>            ]"
 cd rust/primefinder/
 cargo build --release --quiet
 cd ../..
 
+clear
 echo "Running Rust benchmark..."
-sleep 1
+echo "[=====>           ]"
+./rust/primefinder/target/release/primefinder -l $max_num > /dev/null
 
+clear
+echo "Running Rust benchmark..."
+echo "[======>          ]"
 ./rust/primefinder/target/release/primefinder -l $max_num > /dev/null
-./rust/primefinder/target/release/primefinder -l $max_num > /dev/null
+
+clear
+echo "Running Rust benchmark..."
+echo "[=======>         ]"
 \time -o rust.txt -f "%C\nTime: %e\n%P\n" ./rust/primefinder/target/release/primefinder -l $max_num
 
 # -- C++ --
+clear
 echo "Compiling C++ benchmark..."
-sleep 1
+echo "[========>        ]"
 g++ -O3 c/findprimes.cpp -o c/findprimes
 
+clear
 echo "Running C++ benchmark..."
-sleep 1
+echo "[=========>       ]"
 ./c/findprimes $max_num > /dev/null
+
+clear
+echo "Running C++ benchmark..."
+echo "[==========>      ]"
 ./c/findprimes $max_num > /dev/null
+
+clear
+echo "Running C++ benchmark..."
+echo "[===========>     ]"
 \time -o cpp.txt -f "%C\nTime: %e\n%P\n" ./c/findprimes $max_num
 
 # -- Python -- 
+clear
 echo "Running Python benchmark..."
+echo "[============>    ]"
 python3 python/findprimes.py $max_num > /dev/null
+
+clear
+echo "Running Python benchmark..."
+echo "[=============>   ]"
 python3 python/findprimes.py $max_num > /dev/null
+
+clear
+echo "Running Python benchmark..."
+echo "[==============>  ]"
 \time -o python.txt -f "%C\nTime: %e\n%P\n" python3 python/findprimes.py $max_num
 
 # -- JavaScript --
+clear
 echo "Running JS benchmark..."
+echo "[==============>  ]"
 node $(pwd)/javascript/findprimes.js $max_num > /dev/null
+
+clear
+echo "Running JS benchmark..."
+echo "[===============> ]"
 node $(pwd)/javascript/findprimes.js $max_num > /dev/null
+
+clear
+echo "Running JS benchmark..."
+echo "[================>]"
 \time -o js.txt -f "%C\nTime: %e\n%P\n" node $(pwd)/javascript/findprimes.js $max_num
+
+clear
+echo "Compiling results"
+echo "[=================]"
+sleep 1
 
 java=$(cat java.txt | grep Time)
 rust=$(cat rust.txt | grep Time)
