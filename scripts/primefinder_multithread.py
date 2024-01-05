c0556c61fb24d20f966051e6861953f25be1a833
an0nymoos3 <moosely123@gmail.com>
2024-01-06 00:34:05 +0100

    rewrote some stuff to be prettier and work on my machine. need to fix progress bar race conditions due to multithreading

--- scripts/primefinder_multithread.py
+++ scripts/primefinder_multithread.py
@@ -1,71 +1,106 @@
+#!/usr/bin/env python3
+
 import concurrent.futures
 import subprocess
 import time
+import os
 
+# Global variable representing state of program. Used for progress bar.
+BENCHMARK_PROGRESS = 0
+
+# Global variable representing total amount of steps in program. Used for progress bar.
+TOTAL_BENCHMARK_STEPS = 18
 
 def compile_script(source_files):
+    """
+    Compile program.
+    """
+    global BENCHMARK_PROGRESS
+    
+    BENCHMARK_PROGRESS += 1
+    #draw_pretty_progress(f"Compiling: {source_files}")
     subprocess.run(source_files, shell=True)
 
 
 def run_script(script_command):
+    """
+    Time the execution of program.
+    """
+    global BENCHMARK_PROGRESS
+
+    BENCHMARK_PROGRESS += 1
+    #draw_pretty_progress(f"Running: {script_command}")
+
     start_time = time.time()
     subprocess.run(script_command, shell=True)
     end_time = time.time()
     execution_time = end_time - start_time
+
     return execution_time
 
 
+def draw_pretty_progress(progress_text):
+    """
+    Draws progess bar to terminal.
+    """
+    global BENCHMARK_PROGRESS
+    global TOTAL_BENCHMARK_STEPS
+
+    os.system("clear")
+    print(progress_text)
+
+    # Build the progress bar string.
+    bar = "["
+    for i in range(BENCHMARK_PROGRESS):
+        bar += "="
+    bar += ">"
+    for i in range(TOTAL_BENCHMARK_STEPS - BENCHMARK_PROGRESS):
+        bar += " "
+    bar += "]"
+
+    # Print progress bar
+    print(bar)
+
+
 if __name__ == "__main__":
+    os.system("clear")
     max_num = input("How high (max number) would you like to go? ")
 
-    print("Compiling code!")
-    time.sleep(1)
+    execution_time = {"C++": 0,
+                      "Java": 0,
+                      "JavaScript": 0,
+                      "Python": 0,
+                      "Rust": 0}
 
     compile_commands = [
+        "g++ -O3 c/findprimes.cpp -o c/findprimes",
         "javac java/primefinder/src/Main.java", 
-        "cargo rust/primefinder/build --release --quiet", 
-        "g++ -O3 c/findprimes.cpp -o c/findprimes"
-    ]
+        "cargo build --manifest-path rust/primefinder/Cargo.toml --release --quiet"]
 
+    script_commands = [
+        "./c/findprimes {}".format(max_num),
+        "java -cp java/primefinder/src/ Main {}".format(max_num),
+        "node javascript/findprimes.js {}".format(max_num),
+        "python3 python/findprimes.py {}".format(max_num),
+        "./rust/primefinder/target/release/primefinder -l {}".format(max_num)]
+
+    # Compile all compiled languages
     with concurrent.futures.ProcessPoolExecutor() as executor:
         compile_script, compile_commands
 
-    print("Compilation complete, running code!")
-
-    execution_time = [0, 0, 0, 0, 0]
-    n = 0
-    while n < 3:
-        script_commands = [   
-            "java -cp java/primefinder/src Main {}".format(max_num),   
-            "./rust/primefinder/target/release/primefinder -l {}".format(max_num), 
-            "./c/findprimes {}".format(max_num), 
-            "python3 python/findprimes.py {}".format(max_num), 
-            "node javascript/findprimes.js {}".format(max_num)             
-        ]
-
+    # Run all languages 3 times
+    for i in range(3):
         with concurrent.futures.ProcessPoolExecutor() as executor:
             results = list(executor.map(run_script, script_commands))
 
-        j = 0
-        for run_time in results: 
-            execution_time[j] += run_time
-            j += 1
-        
-        n += 1
-        print(n, " rounds completed!")
+        # Add result to language
+        index = 0
+        for language in execution_time: 
+            execution_time[language] += results[index]
+            index += 1
 
-    
-    time.sleep(1)
-    languages = [
-        "Java", 
-        "Rust",
-        "C++", 
-        "Python", 
-        "JavaScript"
-    ]
-
-    i = 0
-    for run_time in execution_time:
-        print(f"{languages[i]}: Execution Time - {run_time/n:.2f} seconds")
+    # Print results
+    for language in execution_time:
+        print(f"{language}: Execution Time - {execution_time[language]/3:.2f} seconds")
         i += 1
 
