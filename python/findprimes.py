9865c9945d400adbe0c75125437ecf9c0a7b7182
an0nymoos3 <moosely123@gmail.com>
2024-01-10 08:37:22 +0100

    rust and python recognize SIGINT

--- python/findprimes.py
+++ python/findprimes.py
@@ -1,7 +1,18 @@
 import math
 import argparse
+import signal
+import sys
 
-def findprimes(limit):
+# Global variables to allow signal_handler to reach them.
+primes = []
+num_cycles = 0
+
+def findprimes(limit) -> None:
+    """
+    Looks for prime numbers up to the limit.
+    """
+    global primes
+
     primes = [2]
     for i in range(3, int(limit), 2):
         is_prime = True
@@ -17,13 +28,35 @@ def findprimes(limit):
         if is_prime:
             primes.append(i)
 
+
+def signal_handler(sig, frame) -> None:
+    """
+    Handles SIGINT/ CTRL+C.
+    """
+    global primes
+    global num_cycles
+
+    print(f"{num_cycles} - {len(primes)}")
+    sys.exit(0)
+
 def main():
+    """
+    main()
+    """
+    global primes
+    global num_cycles
+
     parser = argparse.ArgumentParser()
     parser.add_argument('integers', metavar='N', type=int, nargs='+',
                     help='an upper limit to go to')
     args = parser.parse_args()
 
-    findprimes(args.integers[0])
+    signal.signal(signal.SIGINT, signal_handler)
+
+    while(True):
+        primes.clear()
+        findprimes(args.integers[0])
+        num_cycles += 1
 
 
 if __name__ == "__main__":
