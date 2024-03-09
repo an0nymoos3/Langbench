fd92233823a667e61417fc622db64b73048a3bda
an0nymoos3 <moosely123@gmail.com>
2024-03-09 12:44:42 +0100

    better comments for program logic

--- python/findprimes.py
+++ python/findprimes.py
@@ -7,26 +7,35 @@ import sys
 primes = []
 num_cycles = 0
 
+
 def findprimes(limit) -> None:
     """
     Looks for prime numbers up to the limit.
     """
     global primes
+    global num_cycles
 
-    primes = [2]
+    primes = [2]  # Reset the list of primes
+    # Check all odd numbers as even numbers can't be prime
     for i in range(3, int(limit), 2):
         is_prime = True
         sqrt_i = math.sqrt(i)
-        for _,j in enumerate(primes):
+        for _, j in enumerate(primes):  # Iterate over all already known primes
+            # Exit if prime is larger than sqrt of i as it cannot divide evenly any longer.
             if j > sqrt_i:
                 break
 
+            # If j divides i, i isn't prime.
             if i % j == 0:
                 is_prime = False
                 break
-            
+
+        # Add new prime to list of primes
         if is_prime:
             primes.append(i)
+
+    # Increment number of fully completed cycles
+    num_cycles += 1
 
 
 def signal_handler(sig, frame) -> None:
@@ -36,27 +45,29 @@ def signal_handler(sig, frame) -> None:
     global primes
     global num_cycles
 
+    # Print "number_cycles - current_length_of_primes"
     print(f"{num_cycles} - {len(primes)}", end="")
     sys.exit(0)
+
 
 def main():
     """
     main()
     """
-    global primes
-    global num_cycles
 
+    # Get the upper limit from command line args
     parser = argparse.ArgumentParser()
-    parser.add_argument('integers', metavar='N', type=int, nargs='+',
-                    help='an upper limit to go to')
+    parser.add_argument(
+        "integers", metavar="N", type=int, nargs="+", help="an upper limit to go to"
+    )
     args = parser.parse_args()
 
+    # Set CTRL+C/Sigint handler
     signal.signal(signal.SIGINT, signal_handler)
 
-    while(True):
-        primes.clear()
+    # Keep running until program is manually interrupted
+    while True:
         findprimes(args.integers[0])
-        num_cycles += 1
 
 
 if __name__ == "__main__":
--- rust/primefinder/src/main.rs
+++ rust/primefinder/src/main.rs
@@ -14,16 +14,23 @@ unsafe impl Send for HoldsRawPtr {}
 unsafe impl Sync for HoldsRawPtr {}
 
 unsafe fn find_primes(limit: i32, raw_ptrs: &HoldsRawPtr) {
-    (*raw_ptrs.primes).set_len(1);
+    (*raw_ptrs.primes).set_len(1); // Set length back to one (vec = [2]), without having to reset
+                                   // all elements.
 
     for i in (3..limit).step_by(2) {
+        // Look for all primes
         let mut is_prime: bool = true;
         let inner_limit: f32 = f32::sqrt(i as f32);
 
+        // Only check against known primes
         for prime in (*raw_ptrs.primes).iter() {
+            // Any number larger that sqrt(i) cannot divide i
+            // evenly.
             if *prime as f32 > inner_limit {
                 break;
             }
+
+            // If remainder is 0, i is not prime
             if i % prime == 0 {
                 is_prime = false;
                 break;
@@ -34,10 +41,13 @@ unsafe fn find_primes(limit: i32, raw_ptrs: &HoldsRawPtr) {
             (*raw_ptrs.primes).push(i);
         }
     }
+
+    // Increase number of cycles successfully run
     *raw_ptrs.num_cycles += 1;
 }
 
 fn main() {
+    // Get command line args
     let args: Vec<String> = env::args().collect::<Vec<String>>();
     let limit: i32;
 
@@ -47,14 +57,15 @@ fn main() {
         limit = args.get_unchecked(1).parse::<i32>().unwrap();
     }
 
+    // Prepare datastructures needed
     let mut primes: Vec<i32> = vec![2];
-
     let raw_ptrs: HoldsRawPtr = HoldsRawPtr {
         num_cycles: &mut 0,
         primes: &mut primes,
     };
 
     unsafe {
+        // Set CTRL+C/Sigint handler
         ctrlc::set_handler(move || {
             let move_ptrs = raw_ptrs;
             print!("{} - {}", *move_ptrs.num_cycles, (*move_ptrs.primes).len());
@@ -63,6 +74,7 @@ fn main() {
         .expect("Error setting Ctrl-C handler");
     }
 
+    // Run the algorithm over and over until interrupted
     loop {
         unsafe {
             find_primes(limit, &raw_ptrs);
